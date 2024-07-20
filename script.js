2925a7a459617969615c32770fe74ac138bf8365
spahicadis <142044354+spahicadis@users.noreply.github.com>
2024-07-20 18:16:50 +0200

    Left arrow functional

--- script.js
+++ script.js
@@ -1,36 +1,28 @@
-const slideRight = document.querySelector('.right');
-const slideLeft = document.querySelector('.left');
 const slides = document.querySelectorAll('.slide');
-
+const leftArrow = document.querySelector('.left');
+const rightArrow = document.querySelector('.right');
+console.log(slides);
+console.log(leftArrow)
+console.log(rightArrow);
 
 
 slides.forEach(function(element, index) {
-  console.log(element, index);
-/*
-  slides[0].style.transform = `translateX(${index * 0}%)`
-  slides[1].style.transform = `translateX(${index * 100}%)`
-  slides[2].style.transform = `translateX(${index * 200}%)`
-  slides[3].style.transform = `translateX(${index * 300}%)`
-*/
-
-element.style.transform = `translateX(${index * 100}%)`
-  
+  element.style.transform = `translateX(${index * 100}%)`
 })
-let activeIndex = 0;
-slideLeft.addEventListener('click', function() {
-  slides.forEach((element, index) => {
 
+
+let activeElement = 0;
+
+console.log(activeElement);
+
+leftArrow.addEventListener('click', function() {
+  activeElement-=1;
+  slides.forEach((element, index) => {
+   element.style.transform = `translateX(${(activeElement + index) * 100}%)`;
+   //prvi slajd aktiv element 0 + index 1 * 100
+
+    
+    
+    console.log(`Active element is with index ${activeElement}`);
   })
 })
-
-//Ovaj koji je 0 -> - 100%
-//Ovaj koji je 100% -> 0%
-//Ovaj koji je 100% -> 100%
-
-
-
-
-
-
-
-
