b667069ed9be690d39710e93a195ccae9d2fa717
spahicadis <142044354+spahicadis@users.noreply.github.com>
2024-07-21 22:54:26 +0200

    Javascript logic for left sliding and restart after some timeto overcome infinite sliding

--- script.js
+++ script.js
@@ -21,8 +21,28 @@ leftArrow.addEventListener('click', function() {
    element.style.transform = `translateX(${(activeElement + index) * 100}%)`;
    //prvi slajd aktiv element 0 + index 1 * 100
 
+   if(activeElement < -3) {
+    element.style.transform = `translateX(${index * 100}%)`
+    activeElement=0;
+   }
+
+   
+
     
     
     console.log(`Active element is with index ${activeElement}`);
   })
 })
+/*
+rightArrow.addEventListener('click', function() {
+  activeElement-=1;
+  slides.forEach((element, index) => {
+   element.style.transform = `translateX(${(activeElement + index) * 100}%)`;
+   //prvi slajd aktiv element 0 + index 1 * 100
+
+    
+    
+    console.log(`Active element is with index ${activeElement}`);
+  })
+})
+*/
