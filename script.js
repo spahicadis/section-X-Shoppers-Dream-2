afa61b89f529a089a82393e27858574b72c1a75e
spahicadis <142044354+spahicadis@users.noreply.github.com>
2024-07-22 13:26:17 +0200

    Finished version of this project, implemented functionality for left and right sliding

--- script.js
+++ script.js
@@ -6,41 +6,38 @@ console.log(leftArrow)
 console.log(rightArrow);
 
 
-slides.forEach(function(element, index) {
+slides.forEach((element, index) => {
   element.style.transform = `translateX(${index * 100}%)`
 })
-
 
 let activeElement = 0;
 
 console.log(activeElement);
-
 leftArrow.addEventListener('click', function() {
+  activeElement-=3
+  slides.forEach((element, index) => {
+    element.style.transform = `translateX(${(activeElement + index) * 100}%)`;
+
+    if(activeElement < -3) {
+      activeElement+=4
+      element.style.transform = `translateX(${(activeElement + index) * 100}%)`;
+    } 
+  
+   console.log(`${activeElement}, ${index}`)
+  })
+})
+
+rightArrow.addEventListener('click', function() {
   activeElement-=1;
   slides.forEach((element, index) => {
    element.style.transform = `translateX(${(activeElement + index) * 100}%)`;
-   //prvi slajd aktiv element 0 + index 1 * 100
+   
 
    if(activeElement < -3) {
     element.style.transform = `translateX(${index * 100}%)`
     activeElement=0;
    }
 
-    
-    
-    console.log(`Active element is with index ${activeElement}`);
+   console.log(`${activeElement}, ${index}`)
   })
 })
-/*
-rightArrow.addEventListener('click', function() {
-  activeElement-=1;
-  slides.forEach((element, index) => {
-   element.style.transform = `translateX(${(activeElement + index) * 100}%)`;
-   //prvi slajd aktiv element 0 + index 1 * 100
-
-    
-    
-    console.log(`Active element is with index ${activeElement}`);
-  })
-})
-*/
